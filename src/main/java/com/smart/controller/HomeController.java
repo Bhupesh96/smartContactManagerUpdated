f3efd95202870ff6096448a2025ad2d19422a673
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2023-12-07 10:00:57 +0530

    updated

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smart.entities.User;
@@ -27,7 +29,7 @@ public class HomeController {
 		return "Home";
 	}
 
-	@RequestMapping("/about")
+	@RequestMapping("git@github.com:Bhupesh96/smartContactManagerUpdated.git/about")
 	public String About(Model model) {
 		model.addAttribute("title", "About - Smart Contact Manager");
 		return "about";
@@ -41,25 +43,45 @@ public class HomeController {
 	}
 
 	// method for handling user registration
-	@PostMapping("/do_register")
-	public String registerUser(@Valid User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement,
-			HttpSession session, Model model) {
-		try {
-			if (!agreement) {
-				System.out.println("You have not agreed terms and condition");
-				throw new Exception("You have not agreed terms and condition");
-			}
-			System.out.println(agreement);
-			user.setRole("ROLE_USER");
-			user.setEnabled(true);
-			session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
-			userRepository.save(user);
-			return "signup";
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			session.setAttribute("message", new Message("Something went wrong" + e.getMessage(), "alert-danger"));
-			return "signup";
-		}
+	 @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	    public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult bindingResult,
+	                               @RequestParam(value = "agreement", defaultValue = "false") Boolean agreement,
+	                               Model model,
+	                               HttpSession session) {
+
+	        try {
+
+	            if (!agreement) {
+	                System.out.println("You have not agreed the terms & conditions");
+	                throw new Exception("You have not agreed the terms & conditions");
+	            }
+	            if (bindingResult.hasErrors()){
+	                model.addAttribute("user", user);
+	                return "signUp";
+	            }
+	            user.setRole("ROLE_USER");
+	            user.setEnabled(true);
+	            //user.setImageUrl("default.png");
+	           // user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+	            System.out.println("USER: " + user.toString());
+	            System.out.println("AGREEMENT: " + agreement);
+
+	            User savedUser = this.userRepository.save(user);
+
+	            // Show empty user in frontend
+	            model.addAttribute("user", new User());
+
+	            session.setAttribute("message",new Message("Successfully Registered!! ", "alert-success"));
+
+	            return "signUp";
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            model.addAttribute("user", user);
+	            session.setAttribute(
+	                    "message",
+	                    new Message("Something went wrong !! " + e.getMessage(), "alert-danger")
+	            );
+	            return "signUp";
+	        }
 	}
 }
